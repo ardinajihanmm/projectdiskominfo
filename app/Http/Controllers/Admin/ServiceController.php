8752c9b3713fe84ecb90ca071723dfef0961293d
ardinajihanmm <ardinajihan40@gmail.com>
2026-07-02 12:03:58 +0700

    update

--- app/Http/Controllers/Admin/ServiceController.php
+++ app/Http/Controllers/Admin/ServiceController.php
@@ -1,1 +1,15 @@
-{{-- TODO: Implement this view --}}
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Service extends Model
+{
+    protected $fillable = [
+        'nama_layanan',
+        'deskripsi',
+        'sla_hari',
+        'status'
+    ];
+}

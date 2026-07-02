608b27f9919ee51c1d24e124a9e7e5a77cf7e3e7
nayeuuc-afk <nayeeuuc@gmail.com>
2026-07-02 14:29:22 +0700

    update lagi

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -1,2 +1,54 @@
 <?php
-// Template placeholder
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ticket extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'service_id',
+        'kode_ticket',
+        'judul',
+        'deskripsi',
+        'prioritas',
+        'status',
+        'started_at',
+        'completed_at'
+    ];
+
+    /*
+    |--------------------------------------------------------------------------
+    | RELATIONSHIP
+    |--------------------------------------------------------------------------
+    */
+
+    // Ticket dimiliki oleh satu user
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // Ticket memilih satu layanan
+    public function service()
+    {
+        return $this->belongsTo(Service::class);
+    }
+
+    // Ticket memiliki banyak komentar
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    // Ticket memiliki banyak lampiran
+    public function attachments()
+    {
+        return $this->hasMany(Attachment::class);
+    }
+
+}

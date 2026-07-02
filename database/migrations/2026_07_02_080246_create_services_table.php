1ee71a4db537d331be6a3639f564c8e82db54e22
nayeuuc-afk <nayeeuuc@gmail.com>
2026-07-02 15:07:34 +0700

    update migration

---
+++ database/migrations/2026_07_02_080246_create_services_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('department_id')
+                ->constrained('departments')
+                ->cascadeOnDelete();
+
+            $table->string('nama_layanan');
+            $table->text('deskripsi')->nullable();
+            $table->integer('sla')->default(3);
+            $table->boolean('status')->default(true);
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('services');
+    }
+};
---
+++ database/migrations/2026_07_02_080247_create_tickets_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->cascadeOnDelete();
+
+            $table->foreignId('service_id')
+                ->constrained('services')
+                ->cascadeOnDelete();
+
+            $table->string('kode_ticket')->unique();
+            $table->string('judul');
+            $table->text('deskripsi');
+
+            $table->enum('prioritas', [
+                'Rendah',
+                'Sedang',
+                'Tinggi'
+            ])->default('Sedang');
+
+            $table->enum('status', [
+                'To Do',
+                'In Progress',
+                'Done',
+                'Rejected'
+            ])->default('To Do');
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};

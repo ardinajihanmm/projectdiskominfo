0de5e5c198ee3a3a05c9d110ceb0078c70454ecb
ardinajihanmm <ardinajihan40@gmail.com>
2026-07-14 16:08:11 +0700

    update

--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -458,7 +458,7 @@
                         <input
                             type="password"
                             class="form-control"
-                            placeholder="Masukkan password baru"
+                            placeholder="Minimal 6 karakter"
                             id="newPassword"
                             name="password"
                             required>
